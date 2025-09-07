c588f7e17d890a61ed4e1c8a32c7ec377838577d
Ayush Goyal <agoyal2001@gmail.com>
2025-09-07 15:01:15 -0400

    Add basic middleware

---
+++ apps/web/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - api routes
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - _vercel routes
+     * - favicon.* files
+     * - android-chrome.* files
+     * - apple-touch-icon.* files
+     * - manifest.json
+     * - robots.txt
+     * - sitemap.xml
+     * - files ending with .svg, .png, .jpg, .jpeg, .gif, .webp
+     */
+    "/((?!api|_next/static|_next/image|_vercel|favicon.*|android-chrome.*|apple-touch-icon.*|manifest\\.json|robots\\.txt|sitemap\\.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
